b2b2f3aa971e0bc6b6798a6078929a5d70e0c2e5
Mircea Gui <mircea.gui@3pillarglobal.com>
2017-07-18 18:37:11 +0300

    updates to search function
    - removed unnecessary data-list descriptions from .html.
    - search is no longer case sensitive.
    - search matches more comprehensive descriptions.

--- js/search.js
+++ js/search.js
@@ -1,14 +1,14 @@
 // 1. type in the search box
 $("#search").keyup(function(){
 
-  var current_query = $("#search").val();
+  var current_query = $("#search").val().toLowerCase();
 
   if (current_query != ""){
     $("div.img img").hide();
 
     $("div.img img").each(function(){
 
-      var current_list = $(this).attr("data-list");
+      var current_list = $(this).parent().attr("data-title").toLowerCase();
 
       if (current_list.indexOf(current_query) >= 0){
         $(this).show();
